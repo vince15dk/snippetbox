ec4585ca3e806b50a488d24374f4ba88952670a4
Suk Joo Kim <sj@Suks-MacBook-Pro.local>
2021-01-24 19:17:09 +0900

    i2

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -14,7 +16,14 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	if id, err := strconv.Atoi(r.URL.Query().Get("id")); err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+
+	} else {
+		fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	}
+	//w.Write([]byte("Display a specific snippet..."))
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
