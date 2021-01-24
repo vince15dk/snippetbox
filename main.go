e0f72e7addefbd8a1b1e1c706b26e05b525bdec5
Suk Joo Kim <sj@Suks-MacBook-Pro.local>
2021-01-24 19:06:25 +0900

    snippet2

--- main.go
+++ main.go
@@ -18,6 +18,12 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
 	w.Write([]byte("Create a new snippet..."))
 }
 
